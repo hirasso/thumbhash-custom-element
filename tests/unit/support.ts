d30e5775968c4fd263ba8db687be7de5d3f00837
Rasso Hilber <mail@rassohilber.com>
2024-12-21 10:18:13 +0100

    Prevent repeated rendering

--- tests/unit/support.ts
+++ tests/unit/support.ts
@@ -1,1 +1,19 @@
+/** fixtures */
 export const hash = "YTkGJwiQhWUIt4lbgnhZl3asl2BEBGUA";
+export const otherHash = "H5cJDYJXd3lwiXeHd3h4h0yA9VNH";
+
+/** querySelector shorthand */
+export function $<T extends HTMLElement = HTMLElement>(
+  selector: string,
+  container: Document | HTMLElement | ShadowRoot = document,
+) {
+  return container.querySelector<T>(selector);
+}
+
+/** querySelectorAll shorthand */
+export function $$<T extends HTMLElement = HTMLElement>(
+  selector: string,
+  container: Document | HTMLElement | ShadowRoot = document,
+) {
+  return [...container.querySelectorAll<T>(selector)];
+}
