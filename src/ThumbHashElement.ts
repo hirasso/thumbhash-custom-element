365910ad7392922def2bf0a8c3f1a46d160a002a
Rasso Hilber <mail@rassohilber.com>
2025-01-09 19:36:39 +0100

    Add `display: block` to canvas and img

--- src/ThumbHashElement.ts
+++ src/ThumbHashElement.ts
@@ -138,6 +138,7 @@ export default class ThumbHashElement extends HTMLElement {
 
     canvas.style.width = "100%";
     canvas.style.height = "100%";
+    canvas.style.display = "block";
 
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
@@ -171,6 +172,7 @@ export default class ThumbHashElement extends HTMLElement {
     const image = document.createElement("img");
     image.style.width = "100%";
     image.style.height = "100%";
+    image.style.display = "block";
     image.alt = "";
     image.src = getDataURI(hash);
 
